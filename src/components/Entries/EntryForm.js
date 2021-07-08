e12dd093325f9491ea4f7426e1415eecfa28880a
alec dye <alecdye@gmail.com>
2021-07-08 09:27:07 -0700

    Complete form styles

--- src/components/Entries/EntryForm.js
+++ src/components/Entries/EntryForm.js
@@ -2,8 +2,23 @@ const EntryForm = (props) => {
   return (
     <form className="entry-form card">
       <div className="form-upper">
-        {/* glucose box */}
-        {/* time & date stacked inputs */}
+        <div className="glucose flow">
+          <label htmlFor="glucose" className="glucose-label">
+            <span>Blood</span>
+            <span>Sugar:</span>
+          </label>
+          <input type="tel" placeholder="0" className="glucose-input" min="0" max="700"/>
+          <small className="glucose-error">errorMsg</small>
+          <small className="glucose_unit">mg / dL</small>
+        </div>
+        <div className="form-info">
+          <label htmlFor="time">Time:</label>
+          <input type="time" step="min" defaultValue="00:00"/>
+          <small>errorMsg</small>
+          <label htmlFor="date">Date:</label>
+          <input type="date" min="2021-01-02"/>
+          <small>errorMsg</small>
+        </div>
       </div>
       <div className="form-notes">
         <label htmlFor="notes">Notes:</label>
