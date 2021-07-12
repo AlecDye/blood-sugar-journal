f5041e03f7dab9077abfbfbe11c6a927beb33fda
alec dye <alecdye@gmail.com>
2021-07-12 08:31:52 -0700

    Add modal show / hide

--- src/components/Entries/EntryForm.js
+++ src/components/Entries/EntryForm.js
@@ -1,38 +1,42 @@
+import Modal from "../UI/Modal"
+
 const EntryForm = (props) => {
   return (
-    <form className="entry-form card">
-      <div className="form-upper">
-        <div className="glucose flow">
-          <label htmlFor="glucose" className="glucose-label">
-            <span>Blood</span>
-            <span>Sugar:</span>
-          </label>
-          <input type="tel" placeholder="0" className="glucose-input" min="0" max="700"/>
-          <small className="glucose-error">errorMsg</small>
-          <small className="glucose_unit">mg / dL</small>
+    <Modal onCancel={props.onCancel}>
+      <form className="entry-form card">
+        <div className="form-upper">
+          <div className="glucose flow">
+            <label htmlFor="glucose" className="glucose-label">
+              <span>Blood</span>
+              <span>Sugar:</span>
+            </label>
+            <input type="tel" placeholder="0" className="glucose-input" min="0" max="700"/>
+            <small className="glucose-error">errorMsg</small>
+            <small className="glucose_unit">mg / dL</small>
+          </div>
+          <div className="form-info">
+            <label htmlFor="time">Time:</label>
+            <input type="time" step="min" defaultValue="00:00"/>
+            {/* <small>errorMsg</small> */}
+            <label htmlFor="date">Date:</label>
+            <input type="date" min="2021-01-01" defaultValue="2021-01-01"/>
+            {/* <small>errorMsg</small> */}
+          </div>
         </div>
-        <div className="form-info">
-          <label htmlFor="time">Time:</label>
-          <input type="time" step="min" defaultValue="00:00"/>
-          <small>errorMsg</small>
-          <label htmlFor="date">Date:</label>
-          <input type="date" min="2021-01-01" defaultValue="2021-01-01"/>
-          <small>errorMsg</small>
+        <div className="form-notes">
+          <label htmlFor="notes">Notes:</label>
+          <textarea name="notes" id="notes" placeholder="What did you eat?"></textarea>
+          {/* <small>errorMsg</small> */}
         </div>
-      </div>
-      <div className="form-notes">
-        <label htmlFor="notes">Notes:</label>
-        <textarea name="notes" id="notes" placeholder="What did you eat?"></textarea>
-        <small>errorMsg</small>
-      </div>
-      <div className="btn-row">
-        <button className="btn btn-delete" type="button">Delete</button>
-        <div>
-          <button className="btn btn-outline" type="button" onClick={props.onCancel}>Cancel</button>
-          <button className="btn btn-primary" type="submit">Save</button>
+        <div className="btn-row">
+          <button className="btn btn-delete" type="button">Delete</button>
+          <div>
+            <button className="btn btn-outline" type="button" onClick={props.onCancel}>Cancel</button>
+            <button className="btn btn-primary" type="submit">Save</button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </Modal>
   )
 }
 
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom"
 
 const Backdrop = (props) => {
   return (
-    <div className="backdrop"></div>
+    <div className="backdrop" onClick={props.onCancel}></div>
   )
 }
 
@@ -19,7 +19,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onCancel={props.onCancel}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
     </>
   )
